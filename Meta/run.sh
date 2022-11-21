8939fb5b3f64b62eb529ecc6c3b8e381161eb14d
Edward Banner <edward.banner@gmail.com>
2022-11-21 17:43:50 -0500

    Update run.sh

--- Meta/run.sh
+++ Meta/run.sh
@@ -12,7 +12,7 @@ SCRIPT_DIR="$(dirname "${0}")"
 
 # https://www.shellcheck.net/wiki/SC1090 No need to shellcheck private config.
 # shellcheck source=/dev/null
-[ -x "$SCRIPT_DIR/../run-local.sh" ] && . "$SCRIPT_DIR/../run-local.sh"
+[ -x "$SCRIPT_DIR/../run-local.sh" ] && . "$SCRIPT_DIR/../run-local.sh" 
 
 #SERENITY_PACKET_LOGGING_ARG="-object filter-dump,id=hue,netdev=breh,file=e1000.pcap"
 
